4aa68d4b52b5f20c0396fba3bb631bbcf05c3858
IgorSilvaZZ <igor06silva12@gmail.com>
2022-08-01 12:47:47 -0300

    Done Class code-drop

--- public/chat.js
+++ public/chat.js
@@ -4,3 +4,60 @@ const urlSearch = new URLSearchParams(window.location.search);
 
 const username = urlSearch.get("username");
 const room = urlSearch.get("select_room");
+
+function createMessage(data) {
+  const messagesDiv = document.getElementById("messages");
+
+  messagesDiv.innerHTML += `
+      <div class="new_message">
+          <label class="form-label">
+              <strong>${data.username}</strong> <span>${data.text} - ${dayjs(
+    data.createdAt
+  ).format("DD/MM HH:mm")}</span>
+          </label>
+      </div>
+    `;
+}
+
+document.getElementById(
+  "username"
+).innerHTML = `Olá ${username} - Você está na sala: ${room}`;
+
+socket.emit(
+  "select_room",
+  {
+    username,
+    room,
+  },
+  (messages) => {
+    messages.forEach((message) => {
+      createMessage(message);
+    });
+  }
+);
+
+document
+  .getElementById("message_input")
+  .addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+      const text = event.target.value;
+
+      const data = {
+        room,
+        text,
+        username,
+      };
+
+      event.target.value = "";
+
+      socket.emit("message", data);
+    }
+  });
+
+socket.on("message", (data) => {
+  createMessage(data);
+});
+
+document.getElementById("logout").addEventListener("click", () => {
+  window.location.href = "index.html";
+});
--- src/websocket.ts
+++ src/websocket.ts
@@ -1,5 +1,64 @@
 import { io } from "./http";
 
+interface IRoomUser {
+  socket_id: string;
+  username: string;
+  room: string;
+}
+
+interface Message {
+  room: string;
+  text: string;
+  username: string;
+  createdAt: Date;
+}
+
+const users: IRoomUser[] = [];
+
+const messages: Message[] = [];
+
+const getMessagesRoom = (room: string) => {
+  const messagesRoom = messages.filter((message) => message.room === room);
+
+  return messagesRoom;
+};
+
 io.on("connection", (socket) => {
-  console.log(socket.id);
+  socket.on("select_room", (data, callback) => {
+    // Colocar o usuario em alguma sala em especifica na conexão de socket
+    socket.join(data.room);
+
+    const userInRoom = users.find(
+      (user) => user.username === data.username && user.room === data.room
+    );
+
+    if (userInRoom) {
+      userInRoom.socket_id = socket.id;
+    } else {
+      users.push({
+        room: data.room,
+        username: data.username,
+        socket_id: socket.id,
+      });
+    }
+
+    const messagesRoom = getMessagesRoom(data.room);
+
+    callback(messagesRoom);
+  });
+
+  socket.on("message", ({ room, text, username }) => {
+    // Salvar mensagens
+    const message: Message = {
+      room,
+      text,
+      username,
+      createdAt: new Date(),
+    };
+
+    messages.push(message);
+
+    // Enviar para os usuarios da sala especifica
+    io.to(room).emit("message", message);
+  });
 });
